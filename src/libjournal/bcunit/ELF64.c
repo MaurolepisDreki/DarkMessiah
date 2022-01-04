07df328de7f7cbed544715e3a2a75df4ad3d0723
MaurolepisDreki <inventerna@gmail.com>
2022-01-04 04:13:24 -0700

    Renamed tests & added ELF64 Cyclic Shift test

--- src/libjournal/bcunit/ELF64.c
+++ src/libjournal/bcunit/ELF64.c
@@ -29,7 +29,21 @@ void _JournalUnitTests_ELF64_SingleShift_FirstShift() {
 	_Journal_ELF64_digest_char( &hash, data );
 	CU_ASSERT_EQUAL( (char)hash, data );
 	#endif
-}	
+}
+
+/* Test Cyclic Shift */
+void _JournalUnitTests_ELF64_SingleShift_Cycle() {
+	ELF64_Hash hash = 0x0F00000000000000;
+	const ELF64_Hash result = 0x00000000000000F0;
+	const char data = '\0';
+
+	#ifdef NDBUG
+	CU_FAIL( "`Journal_ELF64_digest_char' is an inline function; use `Debug' compile mode." );
+	#else
+	_Journal_ELF64_digest_char( &hash, data );
+	CU_ASSERT_EQUAL( hash, result );
+	#endif
+}
 
 __attribute__((constructor (102)))
 void _JournalUnitTests_RegisterSuite_ELF64() {
@@ -37,8 +51,9 @@ void _JournalUnitTests_RegisterSuite_ELF64() {
 	CU_pTest test;
 
 	suite = CU_add_suite( "[LibJournal] ELF64 SingleShift", NULL, NULL );
-	test = CU_add_test( suite, "Returns Hash", &_JournalUnitTests_ELF64_SingleShift_Return );
-	test = CU_add_test( suite, "First Shift Unchanged", &_JournalUnitTests_ELF64_SingleShift_FirstShift );
+	test = CU_add_test( suite, "Returns Hash Address", &_JournalUnitTests_ELF64_SingleShift_Return );
+	test = CU_add_test( suite, "First Byte Verbatim", &_JournalUnitTests_ELF64_SingleShift_FirstShift );
+	test = CU_add_test( suite, "Left Cyclic Shift", &_JournalUnitTests_ELF64_SingleShift_Cycle );
 	#ifdef NDEBUG
 	CU_set_suite_active( suite, CU_FALSE );
 	#endif
